2217f085a7f829082cf1ad2ad82aac4ecde856cc
s541910 <s541910@nwmissouri.edu>
2021-10-21 17:12:00 -0500

    Connect-Company-Backend

---
+++ src/main/java/com/nwmsu/gdpbackend/GdpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nwmsu.gdpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nwmsu/gdpbackend/GdpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwmsu.gdpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
