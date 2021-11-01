64a26a84db5aadac81289aa060c480abc92bfea9
anusha kanagala <67909209+Anushakanagala@users.noreply.github.com>
2021-10-31 21:04:25 -0500

    created and updated NewApplicationRepository

---
+++ src/main/java/com/nwmsu/gdpbackend/repository/NewApplication.java
@@ -0,0 +1,12 @@
+package com.nwmsu.gdpbackend.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import antlr.debug.Event;
+
+@Repository
+public interface NewApplication extends JpaRepository<Event, Integer> {
+
+}
