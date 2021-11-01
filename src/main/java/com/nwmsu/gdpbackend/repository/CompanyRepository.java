b9887b1f22b50cb4aac92a67db0a91a95ed5609a
akhilkasuvojula <77549243+akhilkasuvojula@users.noreply.github.com>
2021-10-31 21:05:28 -0500

    updated

--- src/main/java/com/nwmsu/gdpbackend/repository/CompanyRepository.java
+++ src/main/java/com/nwmsu/gdpbackend/repository/CompanyRepository.java
@@ -1,9 +1,11 @@
 package com.nwmsu.gdpbackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.nwmsu.gdpbackend.model.Company;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
 }
