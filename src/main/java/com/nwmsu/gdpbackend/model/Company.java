bc9ff3b2121de12c49c1b3851d9bb367724d80b2
akhilkasuvojula <77549243+akhilkasuvojula@users.noreply.github.com>
2021-10-31 20:42:34 -0500

    created entity company and columns in it

--- src/main/java/com/nwmsu/gdpbackend/model/Company.java
+++ src/main/java/com/nwmsu/gdpbackend/model/Company.java
@@ -1,5 +1,38 @@
 package com.nwmsu.gdpbackend.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NaturalId;
+
+
+
+@Entity
+@Table(name = "COMPANY")
 public class Company {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int Company_Id;
+
+	@Column
+	private String Company_Name;
+
+	@Column
+	private String Address;
+
+	@NaturalId
+	@Column
+	private int Telephone_Number;
+
+	@Column
+	private String createdAt;
+     
+	@Column
+	private String updatedAt;
 
 }
