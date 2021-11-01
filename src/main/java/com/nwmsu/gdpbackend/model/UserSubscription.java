d7371f94f3d21a13c1a5267a3cf6f8fb2059f94d
tejaavula <avula.usedu1@gmail.com>
2021-10-31 20:45:04 -0500

    added attributes to subscription

--- src/main/java/com/nwmsu/gdpbackend/model/UserSubscription.java
+++ src/main/java/com/nwmsu/gdpbackend/model/UserSubscription.java
@@ -1,5 +1,31 @@
 package com.nwmsu.gdpbackend.model;
 
-public class UserSubscription {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "usersubscription")
+public class UserSubscription {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "company_id")
+ private int userCompanyId;
+
+public UserSubscription(int userCompanyId) {
+	super();
+	this.userCompanyId = userCompanyId;
+}
+
+public int getUserCompanyId() {
+	return userCompanyId;
+}
+
+public void setUserCompanyId(int userCompanyId) {
+	this.userCompanyId = userCompanyId;
+}
+ 
 }
