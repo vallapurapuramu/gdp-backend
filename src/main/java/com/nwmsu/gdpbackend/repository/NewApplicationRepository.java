970f6fb5a1eceba06a5f787614edc49182d8a95e
s541910 <s541910@nwmissouri.edu>
2021-11-02 16:09:45 -0500

    modified repo

--- src/main/java/com/nwmsu/gdpbackend/repository/NewApplicationRepository.java
+++ src/main/java/com/nwmsu/gdpbackend/repository/NewApplicationRepository.java
@@ -1,12 +1,11 @@
 package com.nwmsu.gdpbackend.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import antlr.debug.Event;
+import com.nwmsu.gdpbackend.model.NewApplication;
 
 @Repository
-public interface NewApplicationRepository extends JpaRepository<Event, Integer> {
+public interface NewApplicationRepository extends JpaRepository<NewApplication, Integer> {
 
 }
