7ed700d4bb7c44d892bb4784a145fb30b54d0812
VENKATESH VEMULA <77640153+Vemula23@users.noreply.github.com>
2021-11-07 21:35:59 -0600

    added mappings in event model

--- src/main/java/com/nwmsu/gdpbackend/model/Event.java
+++ src/main/java/com/nwmsu/gdpbackend/model/Event.java
@@ -10,29 +10,35 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "events")
 public class Event {
-
-	public Event() {
-
-	}
-
-	public Event(long id, String eventName, String eventDescription, String eventType) {
+	public Event(long id, String eventname, String eventdescription, String eventtype, Company company,
+			List<User> users_id) {
 		super();
 		this.id = id;
-		this.EventName = eventName;
-		this.EventDescription = eventDescription;
-		this.EventType = eventType;
+		this.eventname = eventname;
+		this.eventdescription = eventdescription;
+		this.eventtype = eventtype;
+		this.company = company;
+		this.users_id = users_id;
 	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "event_id")
 	private long id;
-	@Column(name = "eventname")
-	private String EventName;
-	@Column(name = "eventdescription")
-	private String EventDescription;
-	@Column(name = "eventtype")
-	private String EventType;
+	@Column(name = "event_name")
+	private String eventname;
+	@Column(name = "event_description")
+	private String eventdescription;
+	@Column(name = "event_type")
+	private String eventtype;
+
+	@ManyToOne(cascade = { CascadeType.ALL })
+	@JsonBackReference
+	@JoinColumn(name = "company_id", nullable = false)
+	private Company company;
+
+	@ManyToMany(mappedBy = "enrolled_events", cascade = { CascadeType.ALL })
+	List<User> users_id;
 
 	public long getId() {
 		return id;
@@ -42,28 +48,27 @@ public class Event {
 		this.id = id;
 	}
 
-	public String getEventName() {
-		return EventName;
+	public String getEventname() {
+		return eventname;
 	}
 
-	public void setEventName(String eventName) {
-		EventName = eventName;
+	public void setEventname(String eventname) {
+		this.eventname = eventname;
 	}
 
-	public String getEventDescription() {
-		return EventDescription;
+	public String getEventdescription() {
+		return eventdescription;
 	}
 
-	public void setEventDescription(String eventDescription) {
-		EventDescription = eventDescription;
+	public void setEventdescription(String eventdescription) {
+		this.eventdescription = eventdescription;
 	}
 
-	public String getEventType() {
-		return EventType;
+	public String getEventtype() {
+		return eventtype;
 	}
 
-	public void setEventType(String eventType) {
-		EventType = eventType;
+	public void setEventtype(String eventtype) {
+		this.eventtype = eventtype;
 	}
-
 }
