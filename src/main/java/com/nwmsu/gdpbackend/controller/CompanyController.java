356fb2d61f956ab11c30c0e045539e630b7a162f
s541910 <s541910@nwmissouri.edu>
2021-12-05 19:03:23 -0600

    added update company api

--- src/main/java/com/nwmsu/gdpbackend/controller/CompanyController.java
+++ src/main/java/com/nwmsu/gdpbackend/controller/CompanyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,44 +40,56 @@ public class CompanyController {
 
 	}
 
-@GetMapping("/companyList")
-public ResponseEntity<List<Company>> getCompnayList() {
-	List<Company> compnaies = companyService.getCompanyList();
-	if (!compnaies.isEmpty()) {
-		return new ResponseEntity<List<Company>>(compnaies, HttpStatus.OK);
-	} else {
-		return new ResponseEntity<List<Company>>(compnaies, HttpStatus.NOT_FOUND);
+	@GetMapping("/companyList")
+	public ResponseEntity<List<Company>> getCompnayList() {
+		List<Company> compnaies = companyService.getCompanyList();
+		if (!compnaies.isEmpty()) {
+			return new ResponseEntity<List<Company>>(compnaies, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<List<Company>>(compnaies, HttpStatus.NOT_FOUND);
+		}
+
 	}
 
-}
+	@GetMapping("/company/{id}")
+	public ResponseEntity<Company> getCompanyById(@PathVariable int id) {
+		Company company = companyService.getCompanyById(id);
+		if (company != null) {
+			return new ResponseEntity<Company>(company, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Company>(company, HttpStatus.NOT_FOUND);
+		}
 
-@GetMapping("/company/{id}")
-public ResponseEntity<Company> getCompanyById(@PathVariable int id) {
-	Company company = companyService.getCompanyById(id);
-	if (company != null) {
-		return new ResponseEntity<Company>(company, HttpStatus.OK);
-	} else {
-		return new ResponseEntity<Company>(company, HttpStatus.NOT_FOUND);
 	}
 
-}
-@DeleteMapping("/deleteCompany/{id}")
-public ResponseEntity<HttpStatus> deleteCompanyById(@PathVariable int id) {
-	companyService.deleteCompanyById(id);
-	Company c = companyService.getCompanyById(id);
-	if (c == null) {
-		return new ResponseEntity<>(HttpStatus.OK);
-	} else {
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	@DeleteMapping("/deleteCompany/{id}")
+	public ResponseEntity<HttpStatus> deleteCompanyById(@PathVariable int id) {
+		companyService.deleteCompanyById(id);
+		Company c = companyService.getCompanyById(id);
+		if (c == null) {
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
-}
-@PostMapping("/add/company")
-public ResponseEntity<HttpStatus> addCompany(@RequestBody Company company) {
-	try {
-		companyService.addCompany(company);
-		return new ResponseEntity<>(HttpStatus.OK);
-	} catch (NoSuchElementException e) {
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+	@PostMapping("/add/company")
+	public ResponseEntity<HttpStatus> addCompany(@RequestBody Company company) {
+		try {
+			companyService.addCompany(company);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (NoSuchElementException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
-}
+
+	@PutMapping("/updateCompany")
+	public ResponseEntity<HttpStatus> updateCompany(@RequestBody Company company) {
+		try {
+			companyService.addCompany(company);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (NoSuchElementException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 }
