4ac071ba4aaa9d3c2c2d8fbd5ec45fa2b7051265
VENKATESH VEMULA <77640153+Vemula23@users.noreply.github.com>
2021-12-05 18:29:36 -0600

    Added Post mapping to the company Controller class

--- src/main/java/com/nwmsu/gdpbackend/controller/CompanyController.java
+++ src/main/java/com/nwmsu/gdpbackend/controller/CompanyController.java
@@ -1,6 +1,7 @@
 package com.nwmsu.gdpbackend.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nwmsu.gdpbackend.model.Company;
@@ -64,6 +67,15 @@ public ResponseEntity<HttpStatus> deleteCompanyById(@PathVariable int id) {
 	if (c == null) {
 		return new ResponseEntity<>(HttpStatus.OK);
 	} else {
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+}
+@PostMapping("/add/company")
+public ResponseEntity<HttpStatus> addCompany(@RequestBody Company company) {
+	try {
+		companyService.addCompany(company);
+		return new ResponseEntity<>(HttpStatus.OK);
+	} catch (NoSuchElementException e) {
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 }
